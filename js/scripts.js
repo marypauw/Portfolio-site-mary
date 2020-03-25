47e8a12274661c105ebaadcda15235a6e8baf07c
marialtal <60838848+marialtal@users.noreply.github.com>
2020-03-25 09:07:37 +0100

    apply changes task 2.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,43 @@
 //This is the start of the javascript file
 console.log("Hello Welcome to my portfolio site!");
+
+//alert "under construction"
+$(".alert-button").on("click", function() {
+  alert("This part of the website is under construction. Come back later!");
+});
+
+//smooth scrolling
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
+
+//smooth scrolling
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
