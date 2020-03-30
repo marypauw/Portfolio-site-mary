aed29994414bbe6d671e5791c283bd76c8152e82
marialtal <60838848+marialtal@users.noreply.github.com>
2020-03-30 13:37:10 +0200

    apply changes task 24 part 1

--- js/scripts.js
+++ js/scripts.js
@@ -41,3 +41,25 @@ $(document).ready(function(){
     } // End if
   });
 });
+
+//Back to the top arrow
+
+//Get the button:
+arrowtop = document.getElementById("arrowtop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    arrowtop.style.display = "block";
+  } else {
+    arrowtop.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
